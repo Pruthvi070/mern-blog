8ef770780bea62880ee4107369a802ca3c296c56
Pruthvi070 <chavanpruthviraj9595@gmail.com>
2024-08-20 17:32:47 +0530

    Create pages And Routes

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,8 +5,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Signin from "./pages/Signin";
-import SignUp from "./pages/Signup";
-import Projecs from "./pages/Projects";
 import Signup from "./pages/Signup";
 import Projects from "./pages/Projects";
 export default function App() {
