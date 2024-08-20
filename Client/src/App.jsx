9c156257e839001985b280850ae08d0be0d7aeb1
Pruthvi070 <chavanpruthviraj9595@gmail.com>
2024-08-20 17:29:45 +0530

    Create pages and Routess

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,10 +1,24 @@
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable no-unused-vars */
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import SignUp from "./pages/Signup";
+import Projecs from "./pages/Projects";
+import Signup from "./pages/Signup";
+import Projects from "./pages/Projects";
 export default function App() {
   return (
-  <div>
-    App
-  </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup/>} />
+        <Route path="/Projects" element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
