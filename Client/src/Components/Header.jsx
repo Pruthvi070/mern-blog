47ee9692bf9a8641b610f2f720ed0c63f0c66396
Pruthvi070 <chavanpruthviraj9595@gmail.com>
2024-08-22 16:52:42 +0530

    Create header Section

--- Client/src/Components/Header.jsx
+++ Client/src/Components/Header.jsx
@@ -1,13 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/no-unknown-property */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unescaped-entities */
 import React from "react";
-import { Button, Navbar, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Navbar, TextInput, Button } from "flowbite-react";
+import { Link, useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
 
 export default function Header() {
-  console.log("Header component rendered"); // Debugging line
+  // Use useLocation hook to get the current path
+  const pathname = useLocation();
 
   return (
     <Navbar className="border-b-2 flex justify-between items-center">
@@ -19,20 +22,36 @@ export default function Header() {
           Pruthvi'sBlog
         </span>
       </Link>
+
       <form className="hidden lg:block">
         <TextInput type="text" placeholder="Search..." icon={AiOutlineSearch} />
       </form>
-      <Button className="w-12 h-10 lg:hidden rounded-full" color="gray">
-        <AiOutlineSearch />
-      </Button>
-      <div className="flex gap-2 items-center">
+
+      <div className="flex gap-2 md:order-2">
         <button className="w-12 h-10 hidden sm:inline-flex items-center justify-center rounded-full bg-gray-200 text-gray-800 dark:bg-gray-700 dark:text-white">
           <FaMoon />
         </button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
+          <Button className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            Sign In
+          </Button>
         </Link>
+        <Navbar.Toggle />
       </div>
+
+      <Navbar.Collapse>
+        <div className="flex space-x-4">
+          <Navbar.Link active={pathname === "/"} as={"div"}>
+            <Link to="/">Home</Link>
+          </Navbar.Link>
+          <Navbar.Link active={pathname === "/about"} as={"div"}>
+            <Link to="/about">About</Link>
+          </Navbar.Link>
+          <Navbar.Link active={pathname === "/projects"} as={"div"}>
+            <Link to="/projects">Projects</Link>
+          </Navbar.Link>
+        </div>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
