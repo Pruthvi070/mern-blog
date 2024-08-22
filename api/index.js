16cc92317d078fe0c1098a3eb4a83ec35b01a6a0
Pruthvi070 <chavanpruthviraj9595@gmail.com>
2024-08-22 19:06:30 +0530

    create user model

--- api/index.js
+++ api/index.js
@@ -7,15 +7,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 mongoose
-  .connect(process.env.MONGO)
-  .then(() => {
-    console.log('MongoDb is connected');
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-  
-  const app = express();
+    .connect(process.env.MONGO)
+    .then(() => {
+        console.log('MongoDb is connected');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+const app = express();
 // Start the server
 app.listen(3000, () => {
     console.log("Server is running on port 3000!");
